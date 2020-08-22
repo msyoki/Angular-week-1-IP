05abe0caadfe98606137437d2c18fc69b37f2204
Msyoki <msyokimutua@gmail.com>
2020-08-22 14:53:29 +0300

    update clock functions for like buttons

--- src/app/likes/likes.component.ts
+++ src/app/likes/likes.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Quote } from '../quote'
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-likes',
   templateUrl: './likes.component.html',
   styleUrls: ['./likes.component.css']
 })
-export class LikesComponent implements OnInit {  
-  @Input()   numberOfLikes : number = 0;
+export class LikesComponent  {  
+  @Input()   
+  numberOfLikes : number = 0;
+  numberOfDislikes : number = 0;
+
   likeButtonClick(){
-    this.numberOfLikes ++;
+    this.numberOfLikes +=1;
   }
   dislikeButtonClick(){
-    this.numberOfLikes --;
+    this.numberOfDislikes +=1;
   }
 
   ngOnInit(): void {
---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit , Output} from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  @Output() quote: Quote;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
