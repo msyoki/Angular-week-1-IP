e1f4bfc12c267e7d8250703279772aa2d16cdb35
Msyoki <msyokimutua@gmail.com>
2020-08-22 22:34:08 +0300

    delete early changes on quote-details component

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -7,7 +7,7 @@ import { Quote } from '../quote'
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-  @Output() quote: Quote;
+  @Output() quotes: Quote;
   constructor() { }
 
   ngOnInit(): void {
