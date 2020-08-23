15f56e57f743215ea449aeb16f6886037c8a0ace
Msyoki <msyokimutua@gmail.com>
2020-08-23 13:09:25 +0300

    backdate default dates for dates argument as test for date counter output

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,9 +10,9 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
     
-    new Quote(1,"Musyoki Mutua",'William W. Purkey','You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.',new Date(2018,3,14)),
-    new Quote(2,"Musyoki Mutua",' Neil Gaiman','Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.',new Date(2012,3,14)),
-    new Quote(3,"Musyoki Mutua",' Eleanor Roosevelt','Do one thing every day that scares you',new Date(2020,3,14)),
+    new Quote(1,"Musyoki Mutua",'William W. Purkey','You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.',new Date(2020,7,22)),
+    new Quote(2,"Musyoki Mutua",' Neil Gaiman','Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.',new Date(2020,7,21)),
+    new Quote(3,"Musyoki Mutua",' Eleanor Roosevelt','Do one thing every day that scares you',new Date(2020,7,20)),
 
   ];
   completeQuote(isComplete, index){
