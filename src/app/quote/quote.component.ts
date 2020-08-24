4208974fb8d5b38e861851676e1531fec421b08e
Msyoki <msyokimutua@gmail.com>
2020-08-24 09:03:56 +0300

    edit default quote text

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,7 @@ export class QuoteComponent implements OnInit {
     
     new Quote(1,"Musyoki Mutua",'William W. Purkey','You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.',new Date(2020,7,22)),
     new Quote(2,"Musyoki Mutua",' Neil Gaiman','Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.',new Date(2020,7,21)),
-    new Quote(3,"Musyoki Mutua",' Eleanor Roosevelt','Do one thing every day that scares you',new Date(2020,7,20)),
+    new Quote(3,"Musyoki Mutua",' Helen Keller','When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.',new Date(2020,7,20)),
 
   ];
   completeQuote(isComplete, index){
@@ -20,13 +20,14 @@ export class QuoteComponent implements OnInit {
       this.quotes.splice(index,1);
     }
   }
-  constructor() { }
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
-    quote.completeDate = new Date(quote.completeDate)
+    quote.postDate = new Date(quote.postDate)
     this.quotes.push(quote)
   }
+  constructor() { }
+
   ngOnInit(): void {
   }
   
